11098f6a942782bf34f07bd4d986fe038f4cb3a0
GabrielVilchis-215460 <al215460@alumnos.uacj.mx>
2026-01-20 20:24:12 -0700

    fix: add fetch latest invoices and card functions in the dashboard page component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,31 +1,50 @@
 import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
-import LastestInvoices from "@/app/ui/dashboard/latest-invoices";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchRevenue } from "@/app/lib/data"; // Función para obtener datos de ingresos
+import {
+  fetchRevenue,
+  fetchLatestInvoices,
+  fetchCardData,
+} from "@/app/lib/data"; // Función para obtener datos de ingresos
 
 // La pagina de Dashboard es un componente de servidor asincrono, esto permite hacer fetch de datos directamente mediante await.
 // Hay 3 componentes que van a recibir datos: Card, RevenueChart y LatestInvoices.
 export default async function DashboardPage() {
   const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
+        {<Card title="Collected" value={totalPaidInvoices} type="collected" />}
+        {<Card title="Pending" value={totalPendingInvoices} type="pending" />}
+        {
+          <Card
+            title="Total Invoices"
+            value={numberOfInvoices}
+            type="invoices"
+          />
+        }
+        {
+          <Card
+            title="Total Customers"
+            value={numberOfCustomers}
+            type="customers"
+          />
+        }
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         {<RevenueChart revenue={revenue} />}
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        {<LatestInvoices latestInvoices={latestInvoices} />}
       </div>
     </main>
   );
