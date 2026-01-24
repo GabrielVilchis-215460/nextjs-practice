f0ea1fa677db2cc26cf3a30f5bc656dd3fc9e69e
GabrielVilchis-215460 <al215460@alumnos.uacj.mx>
2026-01-24 03:32:57 -0700

    feat: add logic for server action to create invoices

---
+++ app/lib/actions.ts
@@ -0,0 +1,43 @@
+"use server";
+import { z } from "zod";
+import postgres from "postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(), // especficamente, la cantidad va cambiar con el coerce de un string, a un numero, mientras que tambien valida su tipo
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100; // buena practica: guardar valores monetarios en centavos
+  const date = new Date().toISOString().split("T")[0]; // crear una nueva fecha en formato "YYYY-MM-DD"
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+  // Test it out:
+  //console.log(rawFormData);
+  /*
+  Output:  GET /dashboard/invoices/create 200 in 1363ms (compile: 9ms, render: 1354ms)
+{
+  customerId: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
+  amount: '69',
+  status: 'pending'
+}
+  */
+}
